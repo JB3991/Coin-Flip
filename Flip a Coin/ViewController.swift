fd88643f61c19da858a03b8fd03d1931e393310c
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2020-11-30 18:39:20 +0000

    Added a head count
    add a counter to add 1 everytime to coin falls on Heads

--- Flip a Coin/ViewController.swift
+++ Flip a Coin/ViewController.swift
@@ -8,27 +8,33 @@
 import UIKit
 
 let outcome = ["HEADS", "TAILS"]
-
+var score: Int = 0
 
 
 class ViewController: UIViewController {
     
+    @IBAction func buttonPressed(_ sender: Any) {
+        label.text = randomResult()
+        scoreLabel.text = "Head count = \(score)"
+        
+        if label.text == "HEADS" {
+            score += 1
+        }
+    }
+    
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func buttonPressed(_ sender: Any) {
-        label.text = randomResult()
     }
     
     func randomResult() -> String {
         let random = arc4random_uniform(UInt32(outcome.count))
         return outcome[Int(random)]
-    }
+        }
 
+
+    
 
 }
-
